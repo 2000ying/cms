b15145113c21d26f72f80971a40b523223cc4672
Apple <apple@AppledeMacBook-Pro.local>
2017-10-28 17:51:18 +0800

    增加登陆拦截器

---
+++ src/main/java/com/meidian/cms/config/InterceptorWebMvcConfigurerAdapter.java
@@ -0,0 +1,15 @@
+package com.meidian.cms.config;
+
+import com.meidian.cms.config.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class InterceptorWebMvcConfigurerAdapter  extends WebMvcConfigurerAdapter {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/login/*");
+        super.addInterceptors(registry);
+    }
+}
